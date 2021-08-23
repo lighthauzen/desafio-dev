1e37b923a7034a1a66f968d9da31a5d3604a8a07
oscar <oscar.casavelha@gmail.com>
2021-08-23 19:45:54 -0300

    estrutura do projeto

---
+++ src/main/java/com/oscar/desafiodevbycoders/DesafioDevBycodersApplication.java
@@ -0,0 +1,13 @@
+package com.oscar.desafiodevbycoders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioDevBycodersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioDevBycodersApplication.class, args);
+	}
+
+}
