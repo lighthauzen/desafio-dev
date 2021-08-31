aadd270ea61ef02d9fc41d90a20e2a5759f090d5
oscar <oscar.casavelha@gmail.com>
2021-08-30 23:43:11 -0300

    readme atualizado

--- src/main/java/com/oscar/desafiodevbycoders/repositories/TiposTransRepository.java
+++
@@ -1,8 +0,0 @@
-package com.oscar.desafiodevbycoders.repositories;
-
-import com.oscar.desafiodevbycoders.models.Tipos_trans;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TiposTransRepository extends JpaRepository<Tipos_trans, Integer> {
-
-}
